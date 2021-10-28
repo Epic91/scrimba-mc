e4ccb2b211519f4a18b1edd427bed3811391ee0b
Epic91 <hernandeze791@gmail.com>
2021-10-27 22:18:01 -0500

    finished js implementation

---
+++ Module3/index.js
@@ -0,0 +1,47 @@
+// elements
+const myEmojis = ["👩‍💻", "🤓", "🏳️‍🌈", "🇲🇽"];
+const emojiContainer = document.getElementById("emoji-container");
+const emojiInput = document.getElementById("emoji-input");
+const pushBtn = document.getElementById("push-btn");
+const unshiftBtn = document.getElementById("unshift-btn");
+const shiftBtn = document.getElementById("shift-btn");
+const popBtn = document.getElementById("pop-btn");
+
+// functions
+
+function renderEmojis() {
+    emojiContainer.innerHTML = "";
+    for(let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span');
+        emoji.textContent = myEmojis[i];
+        emojiContainer.append(emoji);
+    }
+}
+
+renderEmojis();
+
+pushBtn.addEventListener('click', function() {
+    if(emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = "";
+        renderEmojis();
+    }
+})
+
+unshiftBtn.addEventListener('click', function() {
+    if(emojiInput.value){
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = "";
+        renderEmojis();
+    }
+})
+
+shiftBtn.addEventListener('click', function() {
+    myEmojis.shift();
+    renderEmojis();
+})
+
+popBtn.addEventListener('click', function() {
+    myEmojis.pop();
+    renderEmojis();
+})
